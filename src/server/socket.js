ffe4df0b6f4c7bdd36fb43fbc34c0f6a83b9b84d
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2024-12-13 19:19:09 +0100

    consolelog

--- src/server/socket.js
+++ src/server/socket.js
@@ -6,8 +6,8 @@ export function initializeSocket(httpServer) {
   io = new SocketServer(httpServer);
 
   io.on("connection", (socket) => {
-    console.log("Client connected");
-    socket.on("disconnect", () => console.log("Client disconnected"));
+    // console.log("Client connected");
+    // socket.on("disconnect", () => console.log("Client disconnected"));
   });
 
   return io;
